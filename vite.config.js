15bb6c12513128b4c805780aa20e1d660048d78a
Jiahan Li <jcl@JiahandeAir.emma.troy.ny.us>
2025-04-12 21:36:56 -0400

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/random_thoughts/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
       input: {
-        main: '/index.html',
+        main: './index.html',
       },
     },
   },
